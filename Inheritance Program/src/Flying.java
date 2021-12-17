1aec937ae4862581bb8702f720c2587dfbcc94db
Andrew <Andrew@AComputer>
2021-12-17 05:00:24 -0700

    Initial commit

---
+++ Inheritance Program/src/Flying.java
@@ -0,0 +1,8 @@
+public class Flying extends Hobby {
+    public static void haveFun() {
+        System.out.println("I'm having even more fun.");
+    }
+    public static void seeTheWorld() {
+        System.out.println("I get to see the world from above.");
+    }
+}
